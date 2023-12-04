df4195e37e1ec3aac64066d14f146359b5e21b76
MarwanGutin <104581801+MarwanGutin@users.noreply.github.com>
2023-12-04 18:01:55 +0000

    Update changexml.py

--- changexml.py
+++ changexml.py
@@ -19,11 +19,8 @@ parser.add_argument("-x",
                     type=str)
 
 args = parser.parse_args()
-path = os.path.join(args.image_dir)
-
-
-path1 = os.path.join(path, 'train')
-for xml_file in glob.glob(path1 + '/*.xml'):
+path = os.path.join(args.xml_dir)
+for xml_file in glob.glob(path + '/*.xml'):
     tree = ET.parse(xml_file)
     root = tree.getroot()
     for member in root.findall('object'):
@@ -63,42 +60,3 @@ for xml_file in glob.glob(path1 + '/*.xml'):
             member[5][3].text = str(ymn)
     tree.write(xml_file)
 
-path2 = os.path.join(path, 'test')
-for xml_file in glob.glob(path + '/*.xml'):
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
-    for member in root.findall('object'):
-        if member[0].text == 'airplane':
-            print(member[0].text)
-            member[0].text = 'Airplane'
-            print(member[0].text)
-        width = int(root.find('size')[0].text)
-        height = int(root.find('size')[1].text)
-        xmn = int(member[5][0].text)
-        xmx = int(member[5][1].text)
-        ymn = int(member[5][2].text)
-        ymx = int(member[5][3].text)
-
-        if xmn < 0:
-            xmn = 0
-        elif xmn > height:
-            xmn = height
-        if xmx < 0:
-            xmx = 0
-        elif xmx > height:
-            xmx = height
-        if ymn < 0:
-            ymn = 0
-        elif ymn > height:
-            ymn = height
-        if ymx < 0:
-            ymx = 0
-        elif ymx > height:
-            ymx = height
-        if xmn > xmx:
-            member[5][0].text = str(xmx)
-            member[5][1].text = str(xmn)
-        if ymn > ymx:
-            member[5][2].text = str(ymx)
-            member[5][3].text = str(ymn)
-    tree.write(xml_file)
