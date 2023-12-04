d61465120ac5b916907c2fbca11678c2294a0841
MarwanGutin <104581801+MarwanGutin@users.noreply.github.com>
2023-12-04 17:51:31 +0000

    Add files via upload

--- changexml.py
+++ changexml.py
@@ -56,10 +56,29 @@ for xml_file in glob.glob(path + '/*.xml'):
             print(member[0].text)
             member[0].text = 'Airplane'
             print(member[0].text)
-            xmn = int(member[5][0].text)
+        width = int(root.find('size')[0].text)
+        height = int(root.find('size')[1].text)
+        xmn = int(member[5][0].text)
         xmx = int(member[5][1].text)
         ymn = int(member[5][2].text)
         ymx = int(member[5][3].text)
+
+        if xmn < 0:
+            xmn = 0
+        elif xmn > height:
+            xmn = height
+        if xmx < 0:
+            xmx = 0
+        elif xmx > height:
+            xmx = height
+        if ymn < 0:
+            ymn = 0
+        elif ymn > height:
+            ymn = height
+        if ymx < 0:
+            ymx = 0
+        elif ymx > height:
+            ymx = height
         if xmn > xmx:
             member[5][0].text = str(xmx)
             member[5][1].text = str(xmn)
